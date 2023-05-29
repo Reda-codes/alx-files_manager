4c8b673f854887c354ddd7b8b5c727dac1117582
Reda <muhammadredabh@gmail.com>
2023-05-29 18:28:13 +0100

    fixed index route method

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-router.get('/files/:id/publish', FilesController.putPublish);
-router.get('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 module.exports = router;
